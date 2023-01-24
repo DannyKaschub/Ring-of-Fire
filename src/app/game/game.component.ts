61f10bf52f3e45cd1dee9d16bb32b28a219bbd1c
DannyKaschub <dannykashub98@gmail.com>
2023-01-25 00:20:07 +0100

    fehlersuche

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 
 @Component({
@@ -19,8 +19,13 @@ export class GameComponent implements OnInit {
 
   ngOnInit(): void {
       this.newGame();
-      this.firestore.colection('items').valueChanges().subscribe((game) => {console.log('Game update', game);
-    })
+      this
+      .firestore
+      .collection('games')
+      .valueChanges()
+      .subscribe((game) => {
+        console.log('Game update', game);
+      });
   }
 
   newGame() {
